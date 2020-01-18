4949421ee2f0f4e868a67c467b8d8795a90c69ae
Paul Gadi <paulgadi@gmail.com>
2020-01-18 19:46:46 +0800

    refactor for litegraph

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const Chess = require('react-chess')
+import Backend from 'react-dnd-html5-backend'
+//import TouchBackend from 'react-dnd-touch-backend'
+import { DndProvider } from 'react-dnd'
 
-require('./demo.css')
 
-class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props)
-
-    this.state = {pieces: Chess.getDefaultLineup()}
-    this.handleMovePiece = this.handleMovePiece.bind(this)
-  }
-
-  handleMovePiece(piece, fromSquare, toSquare) {
-    const newPieces = this.state.pieces
-      .map((curr, index) => {
-        if (piece.index === index) {
-          return `${piece.name}@${toSquare}`
-        } else if (curr.indexOf(toSquare) === 2) {
-          return false // To be removed from the board
-        }
-        return curr
-      })
-      .filter(Boolean)
-
-    this.setState({pieces: newPieces})
-  }
-
-  render() {
-    const {pieces} = this.state
-    return (
-      <div className="demo">
-        <Chess pieces={pieces} onMovePiece={this.handleMovePiece} />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<DndProvider backend={Backend}><App /></DndProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
