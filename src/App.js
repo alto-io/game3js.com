abb68fe908e954344dca803762fb9b1e4597cb59
Paul Gadi <paulgadi@gmail.com>
2020-01-18 20:04:21 +0800

    replaced react-chess with litegraph

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import LiteGraphJS from 'litegraph.js/build/litegraph.js'
+import 'litegraph.js/css/litegraph.css'
 
 const Chess = require('react-chess')
 
@@ -6,12 +8,47 @@ require('./demo.css')
 
 function App() {
 
+  const [liteGraph, setLiteGraph] = React.useState();
+  const [liteGraphCanvas, setLiteGraphCanvas] = React.useState();
   const [pieces, setPieces] = React.useState(Chess.getDefaultLineup());
 
-  // React.useEffect(()=>{
-  //
-  //
-  // },[])
+  function useWindowSize() {
+    let [size, setSize] = React.useState([0, 0]);
+    React.useLayoutEffect(() => {
+      function updateSize() {
+        setSize([(window.clientWidth||window.scrollWidth||window.innerWidth),(window.clientHeight||window.scrollHeight||window.innerHeight)-8]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+
+  let [width, height] = useWindowSize();
+
+  React.useEffect(()=>{
+
+    var graph = new LiteGraphJS.LGraph();
+
+    //config
+    LiteGraphJS.LiteGraph.debug = true
+
+    var canvas = new LiteGraphJS.LGraphCanvas("#main", graph);
+
+    graph.onAfterExecute = function() {
+      canvas.draw(true);
+    };
+
+    setLiteGraph(graph)
+    setLiteGraphCanvas(canvas)
+
+    setInterval(()=>{
+      //console.log(graph)
+      graph.runStep()
+    },250)
+
+  },[])
 
   function handleMovePiece(piece, fromSquare, toSquare) {
 
@@ -30,10 +67,15 @@ function App() {
   }
 
   return (
-    <div className="demo">
-      <Chess pieces={pieces} onMovePiece={handleMovePiece} />
+    <div id="mainCanvas" style={{position:"relative",overflow:'hidden',background:"#222",width:'100%',height:"100%"}}>
+      <canvas id='main' width={Math.max(100,width)} height={Math.max(100,height)} tabIndex={10} style={{background:"#111111",outline: 'none',borderBottom:'1px solid #666666'}}></canvas>
+      <div id="reactElements"></div>
     </div>
   );
 }
+
+// <div className="demo">
+//   <Chess pieces={pieces} onMovePiece={handleMovePiece} />
+// </div>
 
 export default App;
