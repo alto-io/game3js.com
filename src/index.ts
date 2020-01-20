c5abf0cb5eb26f086f4bbc2fc28cfd754e19fe04
Paul Gadi <paulgadi@gmail.com>
2020-01-20 15:43:16 +0800

    attempted fix for heroku client deploy

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,16 @@ app.use('/api', routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
-  // app.get("*", (req, res) => {
-  //    let url = path.join(__dirname, '../client/build', 'index.html');
-  //    if (!url.startsWith('/app/')) // we're on local windows
-  //    url = url.substring(1);
-  //   res.sendFile(url);
-  // });
+  app.get("*", (req, res) => {
+     let url = path.join(__dirname, '../client/build', 'index.html');
+     if (!url.startsWith('/app/')) // we're on local windows
+     url = url.substring(1);
+    res.sendFile(url);
+  });
 
-  app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  // app.get('*', (req,res) => {
+  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // })
 
 }
 
