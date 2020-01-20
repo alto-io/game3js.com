e65c831cf5ddf4cc0fe1bb27d3032fc19fb057a2
Paul Gadi <paulgadi@gmail.com>
2020-01-20 16:10:23 +0800

    moved to tsc to fix build directory issues

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,19 @@ app.use('/api', routes);
 
 // require('./routes/nakamaRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV != 'production') {
   app.use(express.static('client/build'))
 
-  app.get("*", (req, res) => {
-     let url = path.join(__dirname, '../client/build', 'index.html');
-     if (!url.startsWith('/app/')) // we're on local windows
-     url = url.substring(1);
-    res.sendFile(url);
-  });
+  // app.get("*", (req, res) => {
+  //    let url = path.join(__dirname, '../client/build', 'index.html');
+  //    if (!url.startsWith('/app/')) // we're on local windows
+  //    url = url.substring(1);
+  //   res.sendFile(url);
+  // });
 
-  // app.get('*', (req,res) => {
-  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // })
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 
 }
 
